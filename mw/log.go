31133a5959a7bc44b66994eeaf7b84b73ce05f80
Joan Llopis <jllopisg@gmail.com>
2015-05-18 12:12:37 +0200

    comment to remember to rewrite log mw

--- mw/log.go
+++ mw/log.go
@@ -18,3 +18,25 @@ func LogHandler(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(fn)
 }
+
+// Exemple per posar un logger per defecte i permetre fer-ne servir d'altres alternatius.
+//package authboss
+//
+//import (
+//	"log"
+//	"os"
+//)
+//
+//// DefaultLogger is a basic logger.
+//type DefaultLogger log.Logger
+//
+//// NewDefaultLogger creates a logger to stdout.
+//func NewDefaultLogger() *DefaultLogger {
+//	return ((*DefaultLogger)(log.New(os.Stdout, "", log.LstdFlags)))
+//}
+//
+//// Write writes to the internal logger.
+//func (d *DefaultLogger) Write(b []byte) (int, error) {
+//	((*log.Logger)(d)).Printf("%s", b)
+//	return len(b), nil
+//}
