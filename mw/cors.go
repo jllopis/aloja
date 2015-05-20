8033e362aea9cd7decfd9d12bcd0c23e38fb7f7e
Joan Llopis <jllopisg@gmail.com>
2015-05-20 12:19:24 +0200

    allow cors configuration options

--- mw/cors.go
+++ mw/cors.go
@@ -6,25 +6,13 @@ import (
 	"github.com/rs/cors"
 )
 
-func CorsHandler(h http.Handler) http.Handler {
-	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"localhost", "acb.info"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "OPTIONS", "PATCH", "DELETE"},
-		AllowCredentials: true,
-	})
-	return c.Handler(h)
-	//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	//		// Allow cross domain Ajax requests
-	//		//if r.Method == "OPTIONS" {
-	//		if origin := r.Header.Get("Origin"); origin != "" {
-	//			w.Header().Set("Access-Control-Allow-Origin", origin)
-	//		}
-	//		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, PATCH, DELETE")
-	//		w.Header().Set("Access-Control-Request-Method", "*")
-	//		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Origin, X-Requested-With, Accept, Authorization")
-	//		w.Header().Set("Access-Control-Allow-Credentials", "true")
-	//		//}
-	//		//w.Header().Set("Access-Control-Allow-Origin", "*")
-	//		h.ServeHTTP(w, r)
-	//	})
+type CorsHandler cors.Cors
+
+func NewCors(opts cors.Options) *CorsHandler {
+	c := cors.New(opts)
+	return (*CorsHandler)(c)
+}
+
+func (c *CorsHandler) Handler(h http.Handler) http.Handler {
+	return (*cors.Cors)(c).Handler(h)
 }
